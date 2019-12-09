7fd05b6d4289734a4c98919b5d08399505644b2f
Marco D'Agostino <marcodago@it.ibm.com>
2019-12-09 13:48:03 +0100

    Initial Commit

---
+++ TeamMoods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamMoods
+//
+//  Created by Marco D'Agostino on 09/12/2019.
+//  Copyright © 2019 com.ibm.cio.be.MD.TeamMoods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
